c3f1c0079fd08e4ea8ef68117174d8cde488054f
Subhankar Agarwal <112622171+subhankaragarwal100@users.noreply.github.com>
2025-07-06 12:27:49 +0530

    Initial Commit

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+     getMyFriends,
+     getRecommendedUsers , 
+     sendFriendRequest , 
+     acceptFriendRequest,
+     getFriendRequests,
+     getOutgoingFriendReqs,
+    } from "../controllers/user.controller.js";
+    
+const router = express.Router();
+
+//apply the protectRoute middleware to all routes in this router
+router.use(protectRoute);
+
+router.get("/" , getRecommendedUsers);
+
+router.get("/friends" , getMyFriends);
+
+router.post("/friend-request/:id" , sendFriendRequest);
+
+router.put("/friend-request/:id/accept" , acceptFriendRequest);
+
+router.get("/friend-requests" , getFriendRequests);
+
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
+
+export default router;
